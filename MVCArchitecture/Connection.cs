41864e4269bed4118dadaac169ae4f20e34fb110
muhdanir <muhdanir@gmail.com>
2023-07-13 14:27:08 +0700

    Create Connection.cs

---
+++ MVCArchitecture/Connection.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+
+namespace MVCArchitecture; 
+
+public class Connection
+{
+    private static string _connectionString = "Data Source=DESKTOP-HDKJSS4;Database=db_work;Integrated Security = True; Connect Timeout = 30;";
+
+    private static SqlConnection _connection;
+
+    public static SqlConnection Get()
+    {
+        try
+        {
+            _connection = new SqlConnection(_connectionString);
+            return _connection;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
