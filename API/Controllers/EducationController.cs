b25a86d158d75e56a2de2c7b9972d0bf3724e231
muhdanir <muhdanir@gmail.com>
2023-07-20 14:05:45 +0700

    CURD for Education

---
+++ API/Controllers/EducationController.cs
@@ -0,0 +1,85 @@
+﻿using API.Contracts;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/educations")]
+public class EducationController : ControllerBase
+{
+    private readonly IEducationRepository _educationRepository;
+    public EducationController(IEducationRepository educationRepository)
+    {
+        _educationRepository = educationRepository;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var result = _educationRepository.GetAll();
+        if (!result.Any())
+        {
+            return NotFound();
+        }
+        return Ok(result);
+    }
+
+    [HttpGet("{guid}")]
+    public IActionResult GetByGuid(Guid guid)
+    {
+        var result = _educationRepository.GetByGuid(guid);
+        if(result is null)
+        {
+            return NotFound();
+        }
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public IActionResult Create(Education education)
+    {
+        var result = _educationRepository.Create(education);
+        if (result is null)
+        {
+            return StatusCode(500, "Error Retrieve from database");
+        }
+
+        return Ok(result);
+    }
+
+    [HttpPut]
+    public IActionResult Update(Education education)
+    {
+        var check = _educationRepository.GetByGuid(education.Guid);
+        if(check is null)
+        {
+            return NotFound("Guid is not found");
+        }
+
+        var result = _educationRepository.Update(education);
+        if (!result)
+        {
+            return StatusCode(500, "Error Retrieve from Database");
+        }
+        return Ok("Update Success");
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(Guid guid)
+    {
+        var data = _educationRepository.GetByGuid(guid);
+        if (data is null)
+        {
+            return NotFound("Guid Is Not Found");
+        }
+
+        var result = _educationRepository.Delete(data);
+        if (!result)
+        {
+            return StatusCode(500, "Error Retrieve from Database");
+        }
+        return Ok("Delete Success");
+    }
+
+}
