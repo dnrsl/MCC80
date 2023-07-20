de5278ee2a255f2efd2ac656ada7f9eb7e65ae9e
muhdanir <muhdanir@gmail.com>
2023-07-20 14:53:36 +0700

    Update RoomController.cs

--- API/Controllers/RoomController.cs
+++ API/Controllers/RoomController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers;
 
 [ApiController]
-[Route("api/room")]
+[Route("api/rooms")]
 public class RoomController : ControllerBase
 {
     private readonly IRoomRepository _roomRepository;
