cf17a82173577695be7ca441b91088fe95b14ca8
muhdanir <muhdanir@gmail.com>
2023-07-24 14:30:03 +0700

    Update Validator University

---
+++ API/Utilities/Validations/Universities/NewUniversityValidator.cs
@@ -0,0 +1,16 @@
+﻿using API.DTOs.Universities;
+using FluentValidation;
+
+namespace API.Utilities.Validations.Universities;
+
+public class NewUniversityValidator : AbstractValidator<NewUniversityDto>
+{
+    public NewUniversityValidator()
+    {
+        RuleFor(u => u.Code)
+            .NotEmpty().WithMessage("Code is required");
+
+        RuleFor(u => u.Name)
+            .NotEmpty().WithMessage("Name is required");
+    }
+}
---
+++ API/Utilities/Validations/Universities/UniversityValidator.cs
@@ -0,0 +1,19 @@
+﻿using API.DTOs.Universities;
+using FluentValidation;
+
+namespace API.Utilities.Validations.Universities;
+
+public class UniversityValidator : AbstractValidator<UniversityDto>
+{
+    public UniversityValidator()
+    {
+        RuleFor(u => u.Guid)
+            .NotEmpty().WithMessage("Guid is required");
+
+        RuleFor(u => u.Code)
+            .NotEmpty().WithMessage("Code is required");
+
+        RuleFor(u => u.Name)
+            .NotEmpty().WithMessage("Name is required");
+    }
+}
